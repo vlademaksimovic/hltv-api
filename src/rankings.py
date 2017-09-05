d80198c02ef25f493b203c8ff9c5f7bb164a97bf
Simon Egersand <s.egersand@gmail.com>
2017-09-05 15:55:15 +0200

    If no ranking teams found return 503

--- src/rankings.py
+++ src/rankings.py
@@ -7,6 +7,10 @@ def get(requester):
     parsed_content = requester.request(URL)
 
     teams = parsed_content.find_all('div', attrs={'class': 'ranked-team'})
+
+    if len(teams) == 0:
+        return None
+
     return list(map(_parse_team, teams))
 
 
