62a75dd5f658984a3809f8657337b4b9002a4abd
Simon Egersand <s.egersand@gmail.com>
2017-10-13 23:23:39 +0200

    Log when emitting metric

--- src/metrics.py
+++ src/metrics.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 api_key = os.environ['HOSTEDGRAPHITE_APIKEY']
-url = 'carbon.hostedgraphite.com'
+graphite_connection = ('carbon.hostedgraphite.com', 2003)
 
 allowed_units = ['counter', 'milliseconds', 'code']
 
@@ -14,13 +14,20 @@ allowed_units = ['counter', 'milliseconds', 'code']
 def log(key, unit, value):
     try:
         if unit not in allowed_units:
-            logging.error('Unit [%s] is not an allowed unit type')
+            logging.error('Unit [%s] is not an allowed unit type' % unit)
             return
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         data = '{}.hltv-api.{}.{} {}\n'.format(api_key, key, unit, value)
         data = data.encode('utf-8')
-        sock.sendto(data, (url, 2003))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(data, graphite_connection)
+
+        logger.info(
+            'Emitted metric with key [%s] and value [%s]' % (key, value))
 
     except socket.error:
-        logger.exception('Unable to log metric')
+        logger.exception('Could not emit metric due to socket problems')
+
+    except UnicodeEncodeError:
+        logger.exception(
+            'Unable to encode the following string to UTF-8 [%s]' % data)
