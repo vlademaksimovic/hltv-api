ee95bf7c62f1686d77ebff0201d60410e0f39e5d
Simon Egersand <s.egersand@gmail.com>
2017-10-13 23:48:51 +0200

    Do not emit metrics while developing locally

--- src/metrics.py
+++ src/metrics.py
@@ -5,6 +5,8 @@ import os
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+is_production = bool(os.environ.get('IS_PRODUCTION', default=False))
+
 api_key = os.environ['HOSTEDGRAPHITE_APIKEY']
 graphite_connection = ('carbon.hostedgraphite.com', 2003)
 
@@ -12,15 +14,16 @@ allowed_units = ['counter', 'milliseconds', 'code']
 
 
 def log(key, unit, value):
-    try:
-        if unit not in allowed_units:
-            logging.error('Unit [%s] is not an allowed unit type' % unit)
-            return
+    if unit not in allowed_units:
+        logging.error('Unit [%s] is not an allowed unit type' % unit)
+        return
 
+    try:
         data = '{}.hltv-api.{}.{} {}\n'.format(api_key, key, unit, value)
         data = data.encode('utf-8')
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(data, graphite_connection)
+        if is_production:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.sendto(data, graphite_connection)
 
         logger.info(
             'Emitted metric with key [%s] and value [%s]' % (key, value))
