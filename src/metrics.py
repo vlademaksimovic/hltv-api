9a0d48bd532629fe9e8ef766a269cf0040f4b397
Simon Egersand <s.egersand@gmail.com>
2018-01-01 17:58:17 +0100

    Emit metrics to KeenIO instead of Hosted Graphite

--- src/metrics.py
+++ src/metrics.py
@@ -1,14 +1,11 @@
-import socket
 import logging
 import os
+import keen
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 is_production = bool(os.environ.get('IS_PRODUCTION', default=False))
-
-api_key = os.environ['HOSTEDGRAPHITE_APIKEY']
-graphite_connection = ('carbon.hostedgraphite.com', 2003)
 
 allowed_units = ['counter', 'milliseconds', 'code']
 
@@ -18,19 +15,10 @@ def log(key, unit, value):
         logging.error('Unit [%s] is not an allowed unit type' % unit)
         return
 
-    try:
-        data = '{}.hltv-api.{}.{} {}\n'.format(api_key, key, unit, value)
-        data = data.encode('utf-8')
-        if is_production:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.sendto(data, graphite_connection)
+    if is_production:
+        keen.add_event(key, {
+            unit: value
+        })
 
-        logger.info(
-            'Emitted metric with key [%s] and value [%s]' % (key, value))
-
-    except socket.error:
-        logger.exception('Could not emit metric due to socket problems')
-
-    except UnicodeEncodeError:
-        logger.exception(
-            'Unable to encode the following string to UTF-8 [%s]' % data)
+    logger.info(
+        'Emitted metric with key [%s] and value [%s]' % (key, value))
